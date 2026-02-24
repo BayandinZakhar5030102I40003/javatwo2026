54c9c06899a906ee777f6829c8f72cfcc30ad3ab
BayandinZakhar5030102I40003 <bayandin.za@edu.spbstu.ru>
2026-02-24 23:23:15 +0300

    in_out_task

--- 1task/src/Main.java
+++ 1task/src/Main.java
@@ -9,12 +9,12 @@ interface Flyable {
 class Sparrow extends Bird implements Flyable {
     @Override
     public void eat() {
-        System.out.println("Воробей ест");
+        System.out.println("Воробей кушает");
     }
 
     @Override
     public void fly() {
-        System.out.println("Воробей летит");
+        System.out.println("Воробей летает");
     }
 }
 
@@ -25,11 +25,11 @@ class Penguin extends Bird {
     }
 
     public void swim() {
-        System.out.println("Пингвин плывет");
+        System.out.println("Пингвин плавает");
     }
 }
 
-// Клиентский код
+
 public class Main {
     public static void makeBirdEat(Bird bird) {
         bird.eat();
@@ -47,6 +47,11 @@ public class Main {
         makeBirdEat(penguin);
 
         makeBirdFly((Flyable) sparrow);
-        // makeBirdFly(penguin); // Ошибка компиляции - пингвин не Flyable
+        // makeBirdFly(penguin); // Ошибка компиляции - пингвин не летает
     }
 }
+//Суть принципа
+//Основная формулировка:
+//Объекты в программе должны быть заменяемыми экземплярами их базовых типов без нарушения корректности программы.
+//Иными словами: если S — подтип типа T, то объекты типа T в любой части программы можно заменить объектами типа S,
+// и программа при этом продолжит работать корректно, не выдавая ошибок и не меняя ожидаемого поведения.
